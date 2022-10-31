58f3c591af19c861c28f13f8b23a960ac9d4af0c
jose-santana002 <santana.jcr@protonmail.com>
2022-10-31 16:26:35 -0700

    moved dance.js to public folder

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,7 @@
 import Head from 'next/head'
 import { Suspense } from 'react'
 import { Canvas } from '@react-three/fiber'
-import Dance from './Dance'
+import Dance from '../public/Dance'
 import styles from '../styles/Home.module.css'
 
 export default function Home() {
